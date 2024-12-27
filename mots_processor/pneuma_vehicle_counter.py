082b38fc563dad122579af5a475ff9b4cc1b090f
Samuel Simplicio <samuelsimplicio5@gmail.com>
2024-12-27 19:20:39 -0300

    Get TSE data

--- mots_processor/pneuma_vehicle_counter.py
+++ mots_processor/pneuma_vehicle_counter.py
@@ -5,14 +5,16 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def load_df(mot_file: str) -> pd.DataFrame:
+def load_df(mot_file: str, bottom: bool = False) -> pd.DataFrame:
     df = pd.read_csv(
         mot_file,
         names=["frame", "id", "bb_left", "bb_top", "bb_width", "bb_height", "conf", "x", "y", "z"]
     )
 
-    bottom_df = df[df["bb_top"] > 300]
-    return bottom_df
+    if bottom:
+        df = df[df["bb_top"] > 300]
+
+    return df
 
 
 def passed_through_pos(df: pd.DataFrame, x: int):
@@ -51,7 +53,7 @@ def calc_speeds(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 def calc_tse(df: pd.DataFrame, u: int, spacing: int):
-    df = df.groupby("id").apply(calc_speeds)
+    df = df.groupby("id")[df.columns.tolist()].apply(calc_speeds)
     current_index = 0
     frame_count = df["frame"].max()
     speed_sums = []
@@ -59,7 +61,7 @@ def calc_tse(df: pd.DataFrame, u: int, spacing: int):
     while current_index < frame_count:
         speed_sum = 0
         count = 0
-        current_frame = current_index + 1
+        current_frame = current_index
         last_frame = current_frame + spacing
         while current_frame < last_frame:
             next_frame = current_frame + 1
@@ -71,6 +73,7 @@ def calc_tse(df: pd.DataFrame, u: int, spacing: int):
                 # has already passed through u
                 if last_pos > u:
                     continue
+
 
                 # no new pos
                 next_pos = next_positions[next_positions["id"] == current_pos["id"]]
@@ -85,103 +88,107 @@ def calc_tse(df: pd.DataFrame, u: int, spacing: int):
 
             current_frame += 1
 
-        current_index += spacing
+        current_index = last_frame
         speed_sums.append(speed_sum)
         counts.append(count)
 
     # speeds = [3.6 * s / c if c > 0 else 0 for s, c in zip(speed_sums, counts)]
     # leave everything in pixels
-    speeds = [3600 * s / c if c > 0 else 0 for s, c in zip(speed_sums, counts)]
-    flows = [3600 * c / (spacing * 1 / FPS) for c in counts]
+    speeds = [3.6 * s / c if c > 0 else 0 for s, c in zip(speed_sums, counts)]
+    flows = [3600* c / (spacing * 1 / FPS) for c in counts]
     #dens = [1000 * f / s if s > 0 else 0 for f, s in zip(flows, speeds)]
-    dens = [f / s if s > 0 else 0 for f, s in zip(flows, speeds)]
+    dens = [1000 * f / s if s > 0 else 0 for f, s in zip(flows, speeds)]
     return speeds, flows, dens, counts
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--mot-file')
-    parser.add_argument('-t', '--truth-mot')
-    parser.add_argument('-s', '--spacing', type=int)
-    parser.add_argument('-f', '--filter-speed', action='store_true')
-    args = parser.parse_args()
-    df = load_df(args.mot_file)
+def tse(df_t: pd.DataFrame, mot_file: str, u: int, bottom: bool, results: list):
+    print(f"Evaluating {mot_file}")
 
-    df_t = load_df(args.truth_mot)
-    print("calculating preds")
-    speeds, flows, dens, counts = calc_tse(df, 500, args.spacing)
-    print("calculating true")
-    speeds_t, flows_t, dens_t, counts_t = calc_tse(df_t, 500, args.spacing)
+    df = load_df(mot_file, bottom)
+    speeds, flows, dens, counts = calc_tse(df, u, args.spacing)
+    speeds_t, flows_t, dens_t, counts_t = calc_tse(df_t, u, args.spacing)
 
-    print(sum(counts), sum(counts_t))
-
-    plt.figure()
-    plt.plot(counts, '-o', label="counts")
-    plt.plot(counts_t, '-o', label="true")
-    plt.xlabel("T")
-    plt.ylabel("Num Vehicles")
-    plt.title("Counts")
-    plt.legend()
-    plt.savefig('veh-count.png')
-    plt.show(block=False)
-
-    plt.figure()
-    plt.plot(speeds, '-o', label="speeds")
-    plt.plot(speeds_t, '-o', label="true")
-    plt.xlabel("T")
-    plt.ylabel("Pixels / minute")
-    plt.legend()
-    plt.title("Speeds")
-    plt.savefig('speed.png')
-    plt.show(block=False)
-
-    plt.figure()
-    plt.plot(flows, '-o', label="flows")
-    plt.plot(flows_t, '-o', label="true")
-    plt.xlabel("T")
-    plt.ylabel("Vehicles / hour")
-    plt.title("Flows")
-    plt.legend()
-    plt.savefig('flows.png')
-    plt.show(block=False)
-
-    plt.figure()
-    plt.plot(dens, '-o', label="dens")
-    plt.plot(dens_t, '-o', label="true")
-    plt.xlabel("T")
-    plt.ylabel("Vehicles / Pixels")
-    plt.title("Dens")
-    plt.legend()
-    plt.savefig('dens.png')
-    plt.show(block=False)
-
-    ## show error
-    times = list(range(len(counts)))
     errors = {
         "Counts": [100 * abs(c - counts[i]) / c if c > 0 else 0 for i, c in enumerate(counts_t)],
         "Flows": [100 * abs(f - flows[i]) / f if f > 0 else 0 for i, f in enumerate(flows_t)],
         "Speeds": [100 * abs(s - speeds[i]) / s if s > 0 else 0 for i, s in enumerate(speeds_t)],
         "Densities": [100 * abs(d - dens[i]) / d if d > 0 else 0 for i, d in enumerate(dens_t)],
     }
+    errors_abs = {
+        "Counts": [abs(c - counts[i]) for i, c in enumerate(counts_t)],
+        "Flows": [abs(f - flows[i]) for i, f in enumerate(flows_t)],
+        "Speeds": [abs(s - speeds[i]) for i, s in enumerate(speeds_t)],
+        "Densities": [abs(d - dens[i]) for i, d in enumerate(dens_t)],
+    }
+    errors_sum = {
+        "Counts": 100 * abs(sum(counts_t) - sum(counts)) / sum(counts_t) if sum(counts_t) > 0 else 0,
+        "Flows": 100 * abs(sum(flows) - sum(flows_t)) / sum(flows_t) if sum(counts_t) > 0 else 0,
+        "Speeds": 100 * abs(sum(speeds) - sum(speeds_t)) / sum(speeds_t) if sum(counts_t) > 0 else 0,
+        "Densities": 100 * abs(sum(dens_t) - sum(dens)) / sum(dens_t) if sum(counts_t) > 0 else 0,
+    }
+
     for name, errs in errors.items():
-        print(f"{name} error is {np.mean(errs)}")
+        print(f"{name} relative error is {np.mean(errs)}, +- {np.std(errs)}")
+        results.append(np.mean(errs))
+        results.append(np.std(errs))
+    print("")
+    for name, errs in errors_abs.items():
+        print(f"{name} cummulative error is {np.sum(errs)}, total sum error: {errors_sum[name]}")
+        results.append(np.sum(errs))
+        results.append(errors_sum[name])
+    print("")
+    for name, errs in errors_abs.items():
+        print(f"{name} mean abs error is {np.mean(errs)}, +- {np.std(errs)}")
+        results.append(np.mean(errs))
+        results.append(np.std(errs))
 
-    for att, value in errors.items():
-        plt.figure()
-        x = np.arange(len(errors["Counts"]))  # the label locations
-        width = 0.5  # the width of the bars
+    print("\n\n")
 
-        rects = plt.bar(x, value, width, label=att)
-        plt.bar_label(rects, padding=3)
 
-        # Add some text for labels, title and custom x-axis tick labels, etc.
-        plt.ylabel('Errors (%)')
-        plt.title(f'Relative errors - {att}')
-        plt.xticks(x + width, times)
-        plt.legend(loc='upper left')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--index')
+    parser.add_argument('-s', '--spacing', type=int, default=100)
+    parser.add_argument('-u', '--u', type=int)
+    parser.add_argument('-b', '--bottom', action='store_true')
+    args = parser.parse_args()
 
-        plt.savefig(f'{att.lower()}-error.png')
-        plt.show(block=False)
 
-    input()
+    truth_mot = f"../{args.index}_0900_0930_D10_RM_mot.txt"
+    df_t = load_df(truth_mot, args.bottom)
+
+    results = {}
+
+
+    column_names = []
+
+    names = ["Counts", "Flows", "Speeds", "Densities"]
+    for name in names:
+        column_names.append(f"Relative error mean {name}")
+        column_names.append(f"Relative error std {name}")
+    for name in names:
+        column_names.append(f"Cummulative error mean {name}")
+        column_names.append(f"Cummulative error absolute {name}")
+    for name in names:
+        column_names.append(f"Absolute error mean {name}")
+        column_names.append(f"Absolute error std {name}")
+
+    for kind in ["vel", "acc", "adp"]:
+        for i in range(7, 12):
+            key = f"sort-{kind}-{i}"
+            results[key] = []
+            mot_file = f"../sort-acc/output/pNEUMA{args.index}-{i}-tiny-{kind}.mot"
+            tse(df_t, mot_file, args.u, args.bottom, results[key])
+
+    for tracker in ["idm-tracker", "ByteTrack", "deepsort"]:
+        for i in range(7, 12):
+            key = f"{tracker}-{i}"
+            results[key] = []
+            mot_file = f"../{tracker}/output/pNEUMA{args.index}-{i}-tiny.mot"
+            tse(df_t, mot_file, args.u, args.bottom, results[key])
+
+    res_df = pd.DataFrame(results).T
+    res_df.columns = column_names
+    res_df.to_csv(f"res-{args.index}-{args.u}.csv")
+
 
